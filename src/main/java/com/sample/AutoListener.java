bc22fd7b6dbd817d35173f06542979b2dc865421
kaiyun <kaiyun@zillionfortune.com>
2017-12-06 19:02:00 +0800

    数据库修改

--- src/main/java/com/sample/AutoListener.java
+++ src/main/java/com/sample/AutoListener.java
@@ -7,6 +7,11 @@ import org.activiti.engine.delegate.ExecutionListener;
 
 public class AutoListener implements ExecutionListener {
 
+	/**
+	 * serialVersionUID:TODO(用一句话描述这个变量表示什么).
+	 */
+	private static final long serialVersionUID = 8855662713779080668L;
+
 	@Override
 	public void notify(DelegateExecution execution) throws Exception {
 		// TODO Auto-generated method stub
